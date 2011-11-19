a3e2c034134ef0f83c93cc088b1bf04bc0f26b2b
Calvin Cheng <calvin.cheng@od-eon.com>
2011-11-19 14:18:48 +0800

    v1.2.5 which works with django trunk

---
+++ tests/settings.py
@@ -0,0 +1,22 @@
+# Haystack settings for running tests.
+DATABASES = {
+    'default': {
+        'ENGINE': 'sqlite3',
+        'NAME': 'haystack_tests.db',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'haystack',
+    'core',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+HAYSTACK_SITECONF = 'core.search_sites'
+HAYSTACK_SEARCH_ENGINE = 'dummy'
